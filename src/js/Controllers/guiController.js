799429903961488d8f371f96023f43fd82418d74
MaykeBolt <mayke.silva6@etec.sp.gov.br>
2024-04-04 11:22:30 -0300

    feat: Adicionando o mustache.

--- src/js/Controllers/guiController.js
+++ src/js/Controllers/guiController.js
@@ -1,10 +1,15 @@
+import mustache from "https://cdnjs.cloudflare.com/ajax/libs/mustache.js/4.2.0/mustache.min.js"
 import { Drawer } from "../gui/Drawer.js";
 
 export class guiController {
-    #drawer
+    #guiComponents = {}
     
     buildGui() {
-        this.#drawer = new Drawer("menuDrawer", "btnToggleDrawer");
-        
+        this.#guiComponents.drawer = new Drawer("menuDrawer", "btnToggleDrawer");
+        this.#renderGuiComponents();
+    }
+
+    #renderGuiComponents() {
+        this.#guiComponents.drawer.render();
     }
 }
--- src/js/gui/Drawer.js
+++ src/js/gui/Drawer.js
@@ -1,12 +1,34 @@
+import mustache from "https://cdnjs.cloudflare.com/ajax/libs/mustache.js/4.2.0/mustache.min.js"
 export class Drawer {
     btnToggleDrawerProp;
     drawerProp;
+
+    drawerMenuItens = [
+        {
+            label: "Login",
+            elmId: "drawerMenuItem_Login",
+            action: "open_view_login",
+            //link: ""
+        },
+        {
+            label: "Registrar",
+            elmId: "drawerMenuItem_Register",
+            action: "open_view_register",
+            //link: ""
+        }
+    ]
 
     constructor(drawerId, btnId){
         this.drawerProp = document.getElementById(drawerId);
         this.drawerProp.style.display = "none";
         this.btnToggleDrawerProp = document.getElementById(btnId);
         this.btnToggleDrawerProp.addEventListener('click', () => {this.toggleDrawer()});
+    }
+
+    render() {
+        const template = document.getElementById("DrawerMenuTemplate").innerHTML;
+        const renderedMenu = mustache.render(template, {menuItens: this.drawerMenuItens});
+        this.drawerProp.innerHTML = renderedMenu
     }
 
     #openDrawer() {
